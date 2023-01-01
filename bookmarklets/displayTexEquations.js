e85dc97d9ef1e7c245c34e0778ef0e2fcf26de3b
Bastian Treitz <basti.treitz@gmail.com>
2023-01-01 19:53:47 +0100

    update

--- bookmarklets/displayTexEquations.js
+++ bookmarklets/displayTexEquations.js
@@ -7,6 +7,14 @@ javascript: (function () {
 		}
 	});
 
+	// Check if the MathJax script has already been added to the page
+	var mathJaxScript = document.getElementById("MathJax-script");
+	if (mathJaxScript) {
+		// If the script is already present, remove it
+		mathJaxScript.remove();
+	}
+
+	// Add the MathJax script to the page
 	var script = document.createElement("script");
 	script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
 	script.type = "text/javascript";
